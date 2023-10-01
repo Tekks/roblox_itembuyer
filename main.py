5cca511263bc2c878bcb863797f494887885e23c
Tekks <andreas-heil@hotmail.de>
2023-10-01 19:21:36 +0200

    🔊 added coloized logs

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import pathlib
 import time
 import sqlite3
 import os
-from rich.console import Console
+import logging
 from dotenv import load_dotenv
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 # Env
 load_dotenv()
 cookie = os.getenv("COOKIE")
 idleTimeBetweenRunsInMin = int(os.getenv("IDLE_TIME_BETWEEN_RUNS_IN_MIN"))
 debug = bool(os.getenv("DEBUG") == "true" or os.getenv("DEBUG") == "True")
-
 
 # DB Sqlite
 con = sqlite3.connect("db/items.db")
@@ -24,15 +23,29 @@ cur.execute("CREATE TABLE IF NOT EXISTS items (productId INTEGER PRIMARY KEY)")
 session = requests.Session()
 session.cookies.update({".ROBLOSECURITY": cookie})
 
-console = Console(highlight=False)
+logging.basicConfig(level=logging.DEBUG if debug else logging.INFO,
+                    format='%(asctime)s [%(levelname)s]: %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger()
 
-def logInfo(color: str, type: str, content: str) -> None:
-    console.print(f"[ [bold {color}]{type}[/] ] {content}")
+fgColor = {
+    "black":"\u001b[30m",
+    "red":"\u001b[31m",
+    "green":"\u001b[32m",
+    "yellow":"\u001b[33m",
+    "blue":"\u001b[34m",
+    "magenta":"\u001b[35m",
+    "cyan":"\u001b[36m",
+    "white":"\u001b[37m",
+}
 
-def logDebug(color: str, type: str, content: str) -> None:
-    if debug is False:
-        return
-    logInfo(color, type, content)
+
+def logInfo(color: str, type: str, category: str, content: str) -> None:
+    logger.info(f"{fgColor['white']}[{fgColor[color]}{type}{fgColor['white']}] {fgColor[color]} {category} : {fgColor['white']} {content}")
+
+def logDebug(color: str, type: str, category: str, content: str) -> None:
+    logger.debug(f"{fgColor['white']}[{fgColor[color]}{type}{fgColor['white']}] {fgColor[color]} {category} : {fgColor['white']} {content}")
+
 
 def fetch_items() -> None:
     result = {}
@@ -43,25 +56,25 @@ def fetch_items() -> None:
         res = req.json()
 
         if req.status_code == 429:
-            logInfo("red", "X", "Roblox Rate-Limit reached. Waiting 20 seconds")
+            logInfo("red", "X", "Roblox Rate-Limit reached", "Waiting 20 seconds")
             time.sleep(20)
             continue
 
         for item in res.get("data", []):
-            itemName = item.get("name")
+            itemName = str(item.get("name"))
             itemRestrictions = item.get("itemRestrictions")
             itemId = item.get("productId")
             if "Collectible" in item.get("itemRestrictions"):
-                logDebug("red", "-", f"Item Limited: \"{itemId} :: {itemName}\"")
+                logDebug("red", "-", "Item Limited", f"{itemId} :: {itemName}")
                 continue
 
             cur.execute("SELECT * FROM items WHERE productId=?", (itemId,))
             if cur.fetchone() is not None:
-                logDebug("red", "-", f"Item Already Purchased: \"{itemId} :: {itemName}\"")
+                logDebug("red", "-", "Item Already Purchased", f"{itemId} :: {itemName}")
                 continue
             
             result[itemId] = { "name": itemName, "restrictions": itemRestrictions }
-            logInfo("blue", "?", f"Item Found: \"{itemId} :: {itemName}\"")
+            logInfo("blue", "?", "Item Found", f"{itemId} :: {itemName}")
 
         cursor = res.get("nextPageCursor")
 
@@ -80,7 +93,7 @@ def purchase(productName: str, productId: int) -> None:
         )
 
         if req.status_code == 429:
-            logInfo("red", "X", "Roblox Rate-Limit reached. Waiting 60 seconds")
+            logInfo("red", "X", "Roblox Rate-Limit reached", "Waiting 60 seconds")
             time.sleep(60)
             continue
 
@@ -95,12 +108,12 @@ def purchase(productName: str, productId: int) -> None:
 
 
 def main() -> None:
-    logInfo("yellow", "!", "Roblox ItemBuyer v" + __version__)
+    logInfo("yellow", "!", "Roblox ItemBuyer", f"v {__version__}")
     
     while True:
         freeItems = fetch_items()
         freeItems = { key: value for (key, value) in freeItems.items() if "Collectible" not in value["restrictions"] }
-        logInfo("yellow", "!", f"Found {len(freeItems)} free items")
+        logInfo("yellow", "!", "Found free items", len(freeItems))
         
         currentItemCount = 0
 
@@ -108,13 +121,13 @@ def main() -> None:
             currentItemCount += 1
             result = purchase(item["name"], productId)
             if result == "owned":
-                logDebug("yellow", "-", f"{currentItemCount} / {len(freeItems)} :: Already purchased: \"{item['name']}\"")
+                logDebug("yellow", "-", f"{currentItemCount} / {len(freeItems)}", f"Already purchased: {item['name']}")
             elif result == "purchased":
-                logInfo("green", "+", f"{currentItemCount} / {len(freeItems)} :: Purchased: \"{item['name']}\"")
+                logInfo("green", "+", f"{currentItemCount} / {len(freeItems)}", f"Purchased: {item['name']}")
             time.sleep(2)
 
         formatedDate = time.strftime("%H:%M:%S %d/%m/%Y", time.localtime(time.time() + (idleTimeBetweenRunsInMin * 60)))
-        logInfo("yellow", "!", f"Next run will be at {formatedDate}")
+        logInfo("yellow", "!", f"Next run will be at", {formatedDate})
         time.sleep(idleTimeBetweenRunsInMin * 60)
 
 main()
